19341f8e887a127c8f3f15b2a1b8c1f0850591a0
Logan George <logangeorge@microsoft.com>
2026-02-19 12:46:26 -0800

    fix pv.ps1

--- files/pv.ps1
+++ files/pv.ps1
@@ -1,6 +1,5 @@
 $ProgressPreference='SilentlyContinue'
 iwr lagsoftware.com/files/pv.zip -OutFile ${env:userprofile}/pv.zip
-expand-archive ${env:userprofile}/pv.zip
-rm ${env:userprofile/pv.zip
-& ${env:userprofile}/pv/perfview.exe
-
+expand-archive ${env:userprofile}/pv.zip -destinationpath ${env:userprofile}
+rm ${env:userprofile}/pv.zip
+& ${env:userprofile}/perfview.exe
